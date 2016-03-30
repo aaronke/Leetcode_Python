f05b68fd19c8634ca9a162c9b599d59f3b851125
aaronke <keyahui123@gmail.com>
2016-03-29 21:01:14 -0700

    sqrt 2 binary search

--- Sqrt.py
+++ Sqrt.py
@@ -10,4 +10,19 @@ class Solution(object):
         while abs(start*start-x)>0.01:
             start=(start+x/start)/2
         return int(start)
-        
+    def mySqrt2(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x<=1:
+            return x
+        left=1
+        right=x
+        while left<right:
+            mid=left+(right-left)/2
+            if mid*mid<=x:
+                left=mid+1
+            else:
+                right=mid;
+        return left-1
